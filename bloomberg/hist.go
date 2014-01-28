376468a69f73898060f0f8e7ffed1cb2bf190691
Nicolas Hillegeer <nicolas@hillegeer.com>
2014-01-28 16:46:44 +0100

    bloomberg: improve error messages for hist.go

--- bloomberg/hist.go
+++ bloomberg/hist.go
@@ -21,20 +21,21 @@ type bloomHist struct {
 
 func getHist(symbol string) (*fquery.Hist, error) {
 	url := fmt.Sprintf(HIST_URL, "1Y", symbol)
+	vprintln("bloomberg: fetching historical, ", url)
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("bloomberg: %v, error while fetching, url: %v, error: %v", symbol, url, err)
 	}
 	defer resp.Body.Close()
 
 	dec := json.NewDecoder(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("bloomberg: %v, json new decoder error, url: %v, error: %v", symbol, url, err)
 	}
 
 	var v bloomHist
 	if err := dec.Decode(&v); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("bloomberg: %v, json decode error, url: %v, error: %v", symbol, url, err)
 	}
 
 	var (
